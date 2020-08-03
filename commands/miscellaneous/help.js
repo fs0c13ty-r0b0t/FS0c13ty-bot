068b023e98885b05fc026207cc9705573b2d5310
LucasCtrl <lucasalt@protonmail.com>
2020-08-03 15:44:40 +0200

    Display the API version

--- commands/miscellaneous/help.js
+++ commands/miscellaneous/help.js
@@ -1,6 +1,9 @@
 const { MessageEmbed } = require('discord.js')
-const { prefix, version } = require('../../config.json')
+const { prefix } = require('../../config.json')
+const { version } = require('../../package.json')
 const { black } = require('../../colors.json')
+
+const { getApiVersion } = require('../../models/informations')
 
 module.exports = {
   config: {
@@ -13,13 +16,18 @@ module.exports = {
   },
 
   run: async (bot, message, args) => {
-    let embed = new MessageEmbed()
+    let apiVersion = await getApiVersion().then((version) => {
+      return version
+    })
+
+    let embed = await new MessageEmbed()
       .setColor(black)
       .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL)
       .addField('\u200b', '📍  **INFORMATIONS**', false)
       .addField('Support', 'https://discord.gg/nEDcagb', true)
       .addField('Source code', 'https://github.com/fs0c13ty-r0b0t/FS0c13ty-bot', true)
-      .addField('📁 Version', version, true)
+      .addField('🤖 Bot version', version, true)
+      .addField('📁 API version', apiVersion, true)
       .addField('\u200b', '📍  **COMMANDS LIST**', false)
 
     bot.commands.forEach((c) => {
--- models/informations.js
+++ models/informations.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch')
 
 // Test the connection to the API
 module.exports = {
-  apiInformations() {
+  getApiVersion() {
     return new Promise((resolve, reject) => {
       fetch(`http://localhost:8080/`, {
         headers: { 'access-token': process.env.TOKEN },
       })
         .then((res) => res.json())
         .then((content) => {
-          resolve(content)
+          resolve(content.version)
         })
     })
   },
